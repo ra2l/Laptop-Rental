01ace20502a8a27f24bd92b7ebda8928b45299ef
Ratul saklain <ra2l.cse.b@gmail.com>
2019-08-04 18:12:38 +0600

    styling fix

--- client/src/componants/insertLaptop.jsx
+++ client/src/componants/insertLaptop.jsx
@@ -5,7 +5,7 @@ class insertLaptop extends Component {
     return (
       <form className="container Laptop-form">
         <div className="form-row laptop-form-row">
-          <div className="col-md-2 laptop-form-label">
+          <div className="col-md-4 laptop-form-label">
             <label> Brand</label>
           </div>
           <div className="col-md-4">
@@ -18,7 +18,7 @@ class insertLaptop extends Component {
         </div>
 
         <div className="form-row laptop-form-row">
-          <div className="col-md-2 laptop-form-label">
+          <div className="col-md-4 laptop-form-label">
             <label> Model</label>
           </div>
           <div className="col-md-4">
@@ -31,7 +31,7 @@ class insertLaptop extends Component {
         </div>
 
         <div className="form-row laptop-form-row">
-          <div className="col-md-2 laptop-form-label">
+          <div className="col-md-4 laptop-form-label">
             <label> Screen Size</label>
           </div>
           <div className="col-md-4">
@@ -44,7 +44,7 @@ class insertLaptop extends Component {
         </div>
 
         <div className="form-row laptop-form-row">
-          <div className="col-md-2 laptop-form-label">
+          <div className="col-md-4 laptop-form-label">
             <label> Processor</label>
           </div>
           <div className="col-md-4">
@@ -57,7 +57,7 @@ class insertLaptop extends Component {
         </div>
 
         <div className="form-row laptop-form-row">
-          <div className="col-md-2 laptop-form-label">
+          <div className="col-md-4 laptop-form-label">
             <label> RAM</label>
           </div>
           <div className="col-md-4">
@@ -65,6 +65,66 @@ class insertLaptop extends Component {
               type="text"
               class="form-control"
               placeholder="Insert RAM size"
+            />
+          </div>
+        </div>
+        <div className="form-row laptop-form-row">
+          <div className="col-md-4 laptop-form-label">
+            <label> SSD</label>
+          </div>
+          <div className="col-md-4">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Insert SSD size"
+            />
+          </div>
+        </div>
+        <div className="form-row laptop-form-row">
+          <div className="col-md-4 laptop-form-label">
+            <label> HDD</label>
+          </div>
+          <div className="col-md-4">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Insert HDD size"
+            />
+          </div>
+        </div>
+        <div className="form-row laptop-form-row">
+          <div className="col-md-4 laptop-form-label">
+            <label> GPU</label>
+          </div>
+          <div className="col-md-4">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Insert GPU Model"
+            />
+          </div>
+        </div>
+        <div className="form-row laptop-form-row">
+          <div className="col-md-4 laptop-form-label">
+            <label> Stock</label>
+          </div>
+          <div className="col-md-4">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Insert Stock Amount"
+            />
+          </div>
+        </div>
+        <div className="form-row laptop-form-row">
+          <div className="col-md-4 laptop-form-label">
+            <label> Rent Per Day</label>
+          </div>
+          <div className="col-md-4">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Insert Rent Per Day"
             />
           </div>
         </div>
